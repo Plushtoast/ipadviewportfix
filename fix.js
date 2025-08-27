e8b367f94ed48c646695107034a24936b2d6e948
Plushtoast <janweigmann@web.de>
2025-08-27 08:54:14 +0200

    1.0.6

--- fix.js
+++ fix.js
@@ -1,13 +1,16 @@
-Hooks.on("renderSettings", (app, html, data) => {
-    const button = $(`<button data-tooltip="Set the performance mode to low."><i class="fas fa-bug"></i>iOS Fix</button>`);
-    const location = html.find("#settings-game")
+Hooks.on("renderSettings", (app, jhtml, data) => {
+    const html = $(jhtml);
+    const button = $(`<button type="button" data-tooltip="Set the performance mode to low."><i class="fas fa-bug"></i>iOS Fix</button>`);
+    const location = html.find(".settings");
     location.append(button);
-    button.on('click',async() => { 
-        await game.settings.set("core","performanceMode", 0) 
-        ui.notifications.warn("Performance mode set to low. Reloading the page to apply changes.")
-        setTimeout(() => {
+    button.on('click', () => {
+        const currentFPS = game.settings.get("core","maxFPS");
+        ui.notifications.warn("Performance mode set to low. Reloading the page to apply changes.", { permanent: true});
+        setTimeout(async() => {
+            await game.settings.set("core", "performanceMode", 0);
+            await game.settings.set("core","maxFPS", Math.min(currentFPS, 40));
             window.location.reload();
-        }, 1000)
-        
+        }, 2000)
+
     })
 })
